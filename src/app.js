96088f5113ff140c7bb13748055a3eceeca2d6af
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-09-10 21:13:42 +0500

    feat: updated cors origin in app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CROS_ORIGIN,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
