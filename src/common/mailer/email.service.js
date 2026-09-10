c56aa58e63ad863455a1c5a659a8121a4f0f2905
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-09-10 21:40:40 +0500

    feat: add family 4 for ipv4 connections for sending emails

--- src/common/mailer/email.service.js
+++ src/common/mailer/email.service.js
@@ -13,6 +13,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4, // force IPv4 (fixes ENETUNREACH on Render)
 });
 
 dotenv.config();
